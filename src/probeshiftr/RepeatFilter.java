f3faa512a3c83955499018895f73d5c71357b957
AlexGa <alexander.gabel@helmholtz-hiri.de>
2025-06-16 14:55:23 +0200

    Format code

--- src/probeshiftr/RepeatFilter.java
+++ src/probeshiftr/RepeatFilter.java
@@ -20,12 +20,19 @@ public class RepeatFilter implements OligoFilter{
 	 * @param includeN Ns count as repeats
 	 */
 	public RepeatFilter(double maxRepeatPct, boolean includeLowerCase, boolean includeN) {
+		
 		if(maxRepeatPct < 0 || maxRepeatPct > 1) {
+		
 			throw new IllegalArgumentException("Max repeat percentage must be between 0 and 1");
+		
 		}
+		
 		if(!includeLowerCase && !includeN) {
+		
 			throw new IllegalArgumentException("Must include at least one: lowercase or N");
+		
 		}
+		
 		maxPct = maxRepeatPct;
 		includesLower = includeLowerCase;
 		includesN = includeN;
@@ -36,12 +43,16 @@ public class RepeatFilter implements OligoFilter{
 		
 		int size = oligo.length();
 		int numRepeats = 0;
+		
 		for(int i=0; i<size; i++) {
+			
 			char c = oligo.charAt(i);
+			
 			if(includesLower && !Character.isUpperCase(c)) {
 				numRepeats++;
 				continue;
 			}
+			
 			if(includesN && (c == 'n' || c == 'N')) {
 				numRepeats++;
 				continue;
